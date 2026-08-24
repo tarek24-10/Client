0de6d6454f23572d91624da37f3b1e1c80b8f044
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-08-24 18:21:02 +0300

    Use HTTP Client for call api

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./layout/header/header";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,18 @@ import { Header } from "./layout/header/header";
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
+export class App implements OnInit {
   protected readonly title = 'E-Commerce';
+
+  private baseUrl = "https://localhost:5001/api/";
+  private http = inject(HttpClient);
+  products:any[] = []
+
+  ngOnInit(): void {
+    this.http.get<any>(this.baseUrl + "products").subscribe({
+      next: responce => this.products = responce.value.data,
+      error: error => console.log(error),
+      complete: () => console.log("Complete") 
+    });
+  }
 }
