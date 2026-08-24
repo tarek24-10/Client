4a345f5837cfe6234a8d99afc2fb753b2bcd54d6
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-08-24 05:06:30 +0300

    Adding Angular Material & Tailwind CSS

--- src/app/app.ts
+++ src/app/app.ts
@@ -8,5 +8,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = 'Client';
+  protected readonly title = 'E-Commerce';
 }
