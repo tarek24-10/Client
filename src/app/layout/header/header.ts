8a4df7a07a9fda6015f48237fc9f82204339f381
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-08-24 08:25:31 +0300

    Style header component & use Angular Material Components

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import { MatBadge } from '@angular/material/badge';
+import { MatButton } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [ MatIconModule,
+             MatBadge,
+             MatButton ],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
