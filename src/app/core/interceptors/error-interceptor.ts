83267cd503fc9145e95176f7c2e37dd250f7ff40
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-08-31 18:45:33 +0300

    Create the Server Error page

--- src/app/core/interceptors/error-interceptor.ts
+++ src/app/core/interceptors/error-interceptor.ts
@@ -1,6 +1,6 @@
 import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
-import { Router } from '@angular/router';
+import { NavigationExtras, Router } from '@angular/router';
 import { catchError, throwError } from 'rxjs';
 import { SnackbarService } from '../services/snackbar.service';
 
@@ -35,7 +35,8 @@ export const errorInterceptor: HttpInterceptorFn = (req, next) => {
         router.navigateByUrl('/not-found')
       }
       if(err.status === 500) {
-        router.navigateByUrl('/server-error')
+        const navigaionExtras:NavigationExtras = {state:{error:err.error}};
+        router.navigateByUrl('/server-error',navigaionExtras);
       }
 
       return throwError(() => err);
--- src/app/shared/components/server-error/server-error.ts
+++ src/app/shared/components/server-error/server-error.ts
@@ -1,11 +1,18 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { MatCard } from '@angular/material/card';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-server-error',
-  imports: [],
+  imports: [MatCard],
   templateUrl: './server-error.html',
   styleUrl: './server-error.css',
 })
 export class ServerError {
-
+  error?:any;
+  constructor(private router:Router){
+    const navigation = router.currentNavigation();
+    this.error = navigation?.extras.state?.['error'];
+  }
 }
