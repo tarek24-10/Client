f4535484f73e5460577ee906d40d800ae6e989e9
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-08-31 19:42:26 +0300

    Adding Progress Bar to indicate loading & Making the Header Fixed at the top

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,17 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MatBadge } from '@angular/material/badge';
 import { MatButton } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink, RouterLinkActive } from "@angular/router";
+import { LoadService } from '../../core/services/load.service';
+import { MatProgressBar } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-header',
   imports: [MatIconModule,
     MatBadge,
-    MatButton, RouterLink, RouterLinkActive],
+    MatButton, RouterLink, RouterLinkActive, MatProgressBar],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
-
+  loadService = inject(LoadService);
 }
