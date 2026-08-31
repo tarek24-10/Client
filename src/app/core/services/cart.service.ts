384f5ef63c77c4de08aa3f79c2f18b94a42a26b5
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-08-31 23:43:10 +0300

    Create Cart Service

---
+++ src/app/core/services/cart.service.ts
@@ -0,0 +1,75 @@
+import { inject, Injectable, signal } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Product } from '../../shared/models/product';
+import { CartItem } from '../../shared/models/cartItem';
+import { ShoppingCart } from '../../shared/models/shoppingCart';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CartService {
+  baseUrl = environment.apiUrl;
+  private http = inject(HttpClient);
+  cart = signal<ShoppingCart | null>(null);
+
+  getCart(id:string){
+    return this.http.get<ShoppingCart>(this.baseUrl  + 'cart?id=' + id).subscribe({
+      next: cart => this.cart.set(cart)
+    })
+  }
+
+  setCart(cart:ShoppingCart){
+    return this.http.post<ShoppingCart>(this.baseUrl  + 'cart', cart).subscribe({
+      next: cart => this.cart.set(cart)
+    })
+  }
+
+  addItemToCart(item:Product | CartItem, quantity = 1){
+    const cart = this.cart() ?? this.createCart();
+
+    if(this.isProduct(item)){
+      item = this.mapProductToCartItem(item);
+    }
+
+    cart.items = this.addOrUpdateItem(cart.items, item, quantity);
+
+    this.cart.set(cart);
+  }
+
+  private addOrUpdateItem(items: CartItem[], item: CartItem, quantity: number): CartItem[] {
+    const index = items.findIndex(i => i.productId == item.productId);
+
+    if(index == -1){
+      item.quantity = quantity;
+      items.push(item);
+    }
+    else{
+      items[index].quantity += quantity;
+    }
+
+    return items;
+  }
+
+  private mapProductToCartItem(item: Product): CartItem {
+    return {
+      productId: item.id,
+      productName: item.name,
+      price: item.price,
+      quantity: 0,
+      pictureUrl: item.pictureUrl,
+      brand: item.brand,
+      type: item.type
+    }
+  }
+
+  private isProduct(item:Product | CartItem): item is Product{
+    return (item as Product).id !== undefined;
+  }
+
+  private createCart(): ShoppingCart {
+    const cart = new ShoppingCart();
+    localStorage.setItem('cart_id', cart.id);
+    return cart;
+  }
+}
