4f4ca88e2457b9b1102c6cafac23e46dd179fe5b
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-08-31 22:56:23 +0300

    Create Cart classes

---
+++ src/app/shared/models/cartItem.ts
@@ -0,0 +1,9 @@
+export type CartItem = {
+    productId: number;
+    productName: string;
+    price: number;
+    quantity: number;
+    pictureUrl: string;
+    brand: string;
+    type: string;
+}
