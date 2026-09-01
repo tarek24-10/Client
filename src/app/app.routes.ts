a510cf3fe9573a02eddf59518824e40fb4abd754
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-09-01 20:18:22 +0300

    Create Checkout Component & Service

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,12 +6,14 @@ import { TestError } from './features/test-error/test-error';
 import { ServerError } from './shared/components/server-error/server-error';
 import { NotFound } from './shared/components/not-found/not-found';
 import { Cart } from './features/cart/cart';
+import { Checkout } from './features/checkout/checkout';
 
 export const routes: Routes = [
     {path:'', component:Home},
     {path:'shop', component:Shop},
     {path:'shop/:id', component:ProductDetails},
     {path:'cart', component:Cart},
+    {path:'checkout', component:Checkout},
     {path:'test-error', component:TestError},
     {path:'not-found', component:NotFound},
     {path:'server-error', component:ServerError},
