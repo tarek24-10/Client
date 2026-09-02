69ec641a9d429b6e881d20a43fd1a8eb9b1e2299
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-09-03 00:46:20 +0300

    Creating a Checkout Auth Guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,13 +9,14 @@ import { Cart } from './features/cart/cart';
 import { Checkout } from './features/checkout/checkout';
 import { Login } from './features/account/login/login';
 import { Register } from './features/account/register/register';
+import { authGuard } from './core/guards/auth-guard';
 
 export const routes: Routes = [
     {path:'', component:Home},
     {path:'shop', component:Shop},
     {path:'shop/:id', component:ProductDetails},
     {path:'cart', component:Cart},
-    {path:'checkout', component:Checkout},
+    {path:'checkout', component:Checkout, canActivate: [authGuard]},
     {path:'account/login', component:Login},
     {path:'account/register', component:Register},
     {path:'test-error', component:TestError},
--- src/app/features/account/login/login.ts
+++ src/app/features/account/login/login.ts
@@ -5,7 +5,7 @@ import { MatCard } from '@angular/material/card';
 import { MatInput } from '@angular/material/input';
 import { MatFormField, MatLabel } from '@angular/material/select';
 import { AccountService } from '../../../core/services/account.service';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -19,6 +19,14 @@ export class Login {
   accountService = inject(AccountService);
   private router = inject(Router);
 
+  private activatedRoute = inject(ActivatedRoute);
+  returnUrl = '/shop';
+
+  constructor(){
+    const url = this.activatedRoute.snapshot.queryParams['returnUrl'];
+    if(url) this.returnUrl = url;
+  }
+
   loginForm = this.fb.group({
     email: [''],
     password: ['']
@@ -27,9 +35,8 @@ export class Login {
   onSubmit(){
     this.accountService.login(this.loginForm.value).subscribe({
       next: () => { this.accountService.getUserInfo().subscribe();
-        this.router.navigateByUrl('/shop');
+        this.router.navigateByUrl(this.returnUrl);
       }
-
     })
   }
 }
