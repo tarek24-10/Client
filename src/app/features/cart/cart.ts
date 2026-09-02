6490ff928d50a6c20543ae9c445e1f0d30f1c91b
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-09-03 01:18:07 +0300

    Adding an Empty State componenet

--- src/app/features/cart/cart.ts
+++ src/app/features/cart/cart.ts
@@ -2,10 +2,11 @@ import { Component, inject } from '@angular/core';
 import { CartService } from '../../core/services/cart.service';
 import { ShoppingCartItem } from './shopping-cart-item/shopping-cart-item';
 import { OrderSummary } from "../../shared/components/order-summary/order-summary";
+import { EmptyState } from '../../shared/components/empty-state/empty-state';
 
 @Component({
   selector: 'app-cart',
-  imports: [ShoppingCartItem, OrderSummary],
+  imports: [ShoppingCartItem, OrderSummary, EmptyState],
   templateUrl: './cart.html',
   styleUrl: './cart.css',
 })
---
+++ src/app/shared/components/empty-state/empty-state.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { MatButton } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-empty-state',
+  imports: [MatButton,MatIcon,RouterLink],
+  templateUrl: './empty-state.html',
+  styleUrl: './empty-state.css',
+})
+export class EmptyState {
+
+}
