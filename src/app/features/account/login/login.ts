af368eb2b9a41de21b56dab96ad0dd4993c8d08a
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-09-02 20:26:28 +0300

    Creating the Login Form

--- src/app/features/account/login/login.ts
+++ src/app/features/account/login/login.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { MatCard } from '@angular/material/card';
+import { MatInput } from '@angular/material/input';
+import { MatFormField, MatLabel } from '@angular/material/select';
+import { AccountService } from '../../../core/services/account.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, MatCard, MatFormField, MatLabel, MatInput, MatButton],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+  private fb = inject(FormBuilder)
 
+  accountService = inject(AccountService);
+  private router = inject(Router);
+
+  loginForm = this.fb.group({
+    email: [''],
+    password: ['']
+  });
+
+  onSubmit(){
+    this.accountService.login(this.loginForm.value).subscribe({
+      next: () => { this.accountService.getUserInfo();
+        this.router.navigateByUrl('/shop');
+      }
+
+    })
+  }
 }
