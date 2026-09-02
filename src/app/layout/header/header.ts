3b5272fb5d807123f26d25727f3695ecc193608b
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-09-02 21:27:44 +0300

    Adding the Angular Material Menu

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -7,12 +7,14 @@ import { LoadService } from '../../core/services/load.service';
 import { MatProgressBar } from '@angular/material/progress-bar';
 import { CartService } from '../../core/services/cart.service';
 import { AccountService } from '../../core/services/account.service';
+import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
+import { MatDivider } from '@angular/material/divider';
 
 @Component({
   selector: 'app-header',
   imports: [MatIconModule,
     MatBadge,
-    MatButton, RouterLink, RouterLinkActive, MatProgressBar],
+    MatButton, RouterLink, RouterLinkActive, MatProgressBar, MatMenuTrigger, MatMenu, MatDivider, MatMenuItem],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
