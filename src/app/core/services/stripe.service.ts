fc78bdf48f5f6867eb894973ca9e4907e1cd0049
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-09-10 18:45:33 +0300

    Creating the Address Element

--- src/app/core/services/stripe.service.ts
+++ src/app/core/services/stripe.service.ts
@@ -1,10 +1,11 @@
 import { inject, Injectable } from '@angular/core';
-import {loadStripe, Stripe, StripeElements} from '@stripe/stripe-js'
+import {loadStripe, Stripe, StripeAddressElement, StripeAddressElementOptions, StripeElements} from '@stripe/stripe-js'
 import { environment } from '../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { CartService } from './cart.service';
 import { firstValueFrom, map } from 'rxjs';
 import { ShoppingCart } from '../../shared/models/shoppingCart';
+import { AccountService } from './account.service';
 
 @Injectable({
   providedIn: 'root',
@@ -15,6 +16,8 @@ export class StripeService {
   private baseUrl = environment.apiUrl;
   private cartSerive = inject(CartService);
   private elements?: StripeElements;
+  private addressElement?: StripeAddressElement;
+  private accountService = inject(AccountService);
 
   constructor(){
     this.stripePromise = loadStripe(environment.stripePublicKey);
@@ -32,15 +35,31 @@ export class StripeService {
         this.elements = stripe.elements({clientSecret: cart.clientSecret, appearance:{labels:'floating'}})
       }
       else{
-        throw new Error("Stripe has nt been loaded");
+        throw new Error("Stripe has not been loaded");
       }
     }
 
     return this.elements;
   }
 
+  async createAddressElement(){
+    if(!this.addressElement){
+      const elements = await this.initializeElements();
+      if(elements){
+        const options: StripeAddressElementOptions = {
+          mode:'shipping'
+        };
+        this.addressElement = elements.create('address', options);
+      } else{
+        throw new Error('Elements instance has not been loaded');
+      }
+    }
+    return this.addressElement;
+  }
+
   createOrUpdatePaymentIntenet(){
     const cart = this.cartSerive.cart();
+    console.log(cart);
     if(!cart){
       throw new Error("Problem with cart");
     }
--- src/app/features/checkout/checkout.ts
+++ src/app/features/checkout/checkout.ts
@@ -1,13 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { OrderSummary } from "../../shared/components/order-summary/order-summary";
 import {MatStepperModule} from '@angular/material/stepper';
+import { RouterLink } from "@angular/router";
+import { MatAnchor, MatButton } from "@angular/material/button";
+import { StripeService } from '../../core/services/stripe.service';
+import { StripeAddressElement } from '@stripe/stripe-js';
+import { SnackbarService } from '../../core/services/snackbar.service';
 
 @Component({
   selector: 'app-checkout',
-  imports: [OrderSummary, MatStepperModule],
+  imports: [OrderSummary, MatStepperModule, RouterLink, MatAnchor, MatButton],
   templateUrl: './checkout.html',
   styleUrl: './checkout.css',
 })
-export class Checkout {
+export class Checkout implements OnInit {
+  private stripeSevice = inject(StripeService);
+  private addressElement?: StripeAddressElement;
 
+  private snackbar = inject(SnackbarService);
+
+  async ngOnInit(){
+    try{
+        this.addressElement = await this.stripeSevice.createAddressElement();
+        this.addressElement.mount('#address-element')
+    }
+    catch(error : any){
+      this.snackbar.error(error.message)
+    }
+  }
 }
