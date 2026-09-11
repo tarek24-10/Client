a0b9c59817c9831b2bfe584c348833e6f6ebdb97
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-09-11 18:43:13 +0300

    Updating the Review componenet with Token Information

--- src/app/features/checkout/review/review.ts
+++ src/app/features/checkout/review/review.ts
@@ -1,13 +1,18 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, Input, input } from '@angular/core';
 import { CartService } from '../../../core/services/cart.service';
 import { CurrencyPipe } from '@angular/common';
+import { ConfirmationToken } from '@stripe/stripe-js';
+import { AddressPipe } from '../../../shared/pipes/address-pipe';
+import { CardPipe } from '../../../shared/pipes/card-pipe';
 
 @Component({
   selector: 'app-review',
-  imports: [CurrencyPipe],
+  imports: [CurrencyPipe, AddressPipe, CardPipe],
   templateUrl: './review.html',
   styleUrl: './review.css',
 })
 export class Review {
   cartService = inject(CartService);
+
+  @Input() confirmationToken?:ConfirmationToken;
 }
