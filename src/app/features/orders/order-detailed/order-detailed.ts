a8b677a359aa3e968f768abd62c43b46cb709fda
Tarek Khaled <tarekkhaled5112@gmail.com>
2026-09-13 02:13:22 +0300

    Update the Address & Card Pipes with Type Guards

--- src/app/features/orders/order-detailed/order-detailed.ts
+++ src/app/features/orders/order-detailed/order-detailed.ts
@@ -1,14 +1,16 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { OrderService } from '../../../core/services/order.service';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { Order } from '../../../shared/models/order';
 import { MatCardModule } from '@angular/material/card';
 import { MatButton } from '@angular/material/button';
 import { CurrencyPipe, DatePipe } from '@angular/common';
+import { AddressPipe } from '../../../shared/pipes/address-pipe';
+import { CardPipe } from '../../../shared/pipes/card-pipe';
 
 @Component({
   selector: 'app-order-detailed',
-  imports: [MatCardModule, MatButton, DatePipe, CurrencyPipe],
+  imports: [MatCardModule, MatButton, DatePipe, CurrencyPipe, AddressPipe, CardPipe, RouterLink],
   templateUrl: './order-detailed.html',
   styleUrl: './order-detailed.css',
 })
